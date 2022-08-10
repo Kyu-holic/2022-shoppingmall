85a6aa9bd345f94b39b569bf632adfcaf345dedf
unknown <khhan1990@hanmail.net>
2022-08-10 22:22:45 +0900

    dropzone완성

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, Input } from "antd";
+import FileUpload from "../../utils/FileUpload";
 
 const { Title } = Typography;
 const { TextArea } = Input;
+
+const Continents = [
+  { key: 1, value: "Africa" },
+  { key: 2, value: "Europe" },
+  { key: 3, value: "Asia" },
+  { key: 4, value: "North America" },
+  { key: 5, value: "South America" },
+  { key: 6, value: "Australia" },
+  { key: 7, value: "Antarctica" },
+];
 
 function UploadProductPage() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState(0);
+  const [continent, setContinent] = useState(1);
 
   const titleChangeHandler = (e) => {
     setName(e.target.value);
@@ -21,6 +33,10 @@ function UploadProductPage() {
     setPrice(e.target.value);
   };
 
+  const continentsChangeHandler = (e) => {
+    setContinent(e.target.value);
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -28,6 +44,7 @@ function UploadProductPage() {
       </div>
       <Form>
         {/* DropZone */}
+        <FileUpload />
         <br />
         <br />
         <label>이름</label>
@@ -42,8 +59,12 @@ function UploadProductPage() {
         <Input type="number" onChange={priceChangeHandler} value={price} />
         <br />
         <br />
-        <select>
-          <option></option>
+        <select onChange={continentsChangeHandler} value={continent}>
+          {Continents.map((item) => (
+            <option key={item.key} value={item.value}>
+              {item.value}
+            </option>
+          ))}
         </select>
         <br />
         <br />
